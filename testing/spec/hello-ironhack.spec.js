48490793d648a088c676a536e2d8ec2d867946a9
Jan Redmann <janredmann1@gmail.com>
2021-01-21 12:27:33 +0100

    add testing

--- testing/spec/hello-ironhack.spec.js
+++ testing/spec/hello-ironhack.spec.js
@@ -1,1 +1,10 @@
 // TODO
+describe('Hello ironhack function', function () {
+    it('Should be a function', function () {
+        expect(typeof helloIronhack).toBe('function');
+    });
+    it('Should return greeting', function () {
+        expect(helloIronhack()).toEqual('hello ironhack');
+    })
+})
+
